736df41c2cc28734b1058bc14ba8e0abec4916b9
Davi Azevedo <daviazevedos02@gmail.com>
2023-10-31 22:36:32 -0300

    Melhorando Classes

--- Src/classes.py
+++ Src/classes.py
@@ -1,42 +1,92 @@
 from tupy import*
-from enum import Enum
 import math
 
-
-# class Trajetoria():
-#     def seno(x, y, helper):
-#         helper += 0.1
-#         x += 5
-#         y = (math.sin(helper)*250) + 250
         
 class Projetil(Image):
-    def __init__(self, x, y, image, helper):#, trajetoria):
+    def __init__(self, x, y, image, helper):
         self.file = image
         self.x = x 
         self.y = y
         self.helper = helper
-    def seno(self):
-        self.helper += 0.1
-        self.x -= 10
-        self.y = ((math.sin(self.helper)*250) + 250)
+        self.noAr = False
+    def isOutScreen(self):
+        if self.x < 0:
+            return True
+        if self.x > 800:
+            return True
+        if self.y > 500:
+            return True
+        if self.y < 0:
+            return True
+        else:
+            return False
+    def stopShoot(self):
+        if self.isOutScreen():
+            self.destroy()
 
+        
+class PedraSeno(Projetil):
+    def __init__(self, noAr = False):
+        super().__init__(800, 250, "../Img/pedra_poo.png", -0.2)
+        self.noAr = noAr
+    def shootSeno(super):
+        super.helper += 0.1
+        super.x -= 10
+        super.y = ((math.sin(super.helper)*250) + 250)
+    def update(self):
+        if self.noAr == True:
+            self._show()
+            self.shootSeno()
+            super().stopShoot()
+        else:
+            self._hide()
+
+class FogoReto(Projetil):
+    def __init__(self, noAr = False):
+        super().__init__(800, 250, "../Img/fogo_poo.png", -0.2)
+        self.noAr = noAr
+    def shootReto(super):
+        super.x -=20
+    def update(self):
+        if self.noAr == True:
+            self._show()
+            self.shootReto()
+            super().stopShoot()
+        else:
+            self._hide()
+
+    
 
 class Arma():
-    def __init__(self, qtd_municao, delay, projetil):
-        self.municoes = []
+    def __init__(self, qtd_municao, projetil):
         self.projetil = projetil
-        self.delay = delay
         self.qtd_municao = qtd_municao
-        self.redefine()
-    def atirar(self):
+        self.projetilsDisparados = 0
+        self.atirou = False
+        self.municoes = []
+        for i in range(qtd_municao):
+            x = projetil()
+            self.municoes.append(x)
+        self.currentMuni = [0]
+    def reloadMuni(self):
+        self.municoes.clear()
         for i in range(self.qtd_municao):
-            self.municoes[i].seno()
-    def redefine(self):
-        for i in range(self.qtd_municao):
-            currentDelay = self.delay*i
-            currentX = self.projetil.x +((20*i)/4)
-            self.municoes.append(Projetil(currentX, self.projetil.y, self.projetil.file, self.projetil.helper - currentDelay))
-
+            x = self.projetil()
+            self.municoes.append(x)
+        self.currentMuni = [0]
+    def shoot(self):
+        if self.projetilsDisparados < self.qtd_municao:
+            print("pD < m")
+            self.municoes[self.projetilsDisparados].noAr = True
+            self.projetilsDisparados += 1
+        else:
+            print('pd>')
+            self.atirou = False
+            self.projetilsDisparados = 0
+            self.reloadMuni()
+    def update(self):
+        if self.atirou == True:
+            self.shoot()
 
 class Personagem(Image):
     def __init__(self, x, y, image, life, armas):
@@ -50,21 +100,21 @@ class Vilao_test_1(Personagem):
     def __init__(self, x, y, image, life, armas):
         super().__init__(x, y, image, life, armas)
         self.atirando = False
-        self.currentArma = armas[0]
-    def comecarAtirar(self):
+        self.armas = armas
+    def justShoot(self):
         if keyboard.is_key_just_down('space'):
-            self.atirando = True
+            self.armas[0].atirou = True
+        if keyboard.is_key_just_down('Up'):  
+            self.armas[1].atirou = True
     def update(self):
-        self.comecarAtirar()
-        if self.atirando == True:
-            self.currentArma.atirar()
-            if self.currentArma.municoes[self.currentArma.qtd_municao-1].x < 0:
-                self.currentArma.redefine()
-                
+        self.justShoot()
+        for i in range(len(self.armas)):
+            self.armas[i].update()
 
-pedra = Projetil(800, 250, "../Img/pedra_poo.png", -0.2)
-sennno = Arma(20, 0.05, pedra)
-vi = Vilao_test_1(800, 300, "../Img/balaoVerde_poo.webp", 200, [sennno])
+pedra = Arma(20, PedraSeno)
+fogo = Arma(1, FogoReto)
+
+vi = Vilao_test_1(800, 300, "../Img/balaoVerde_poo.webp", 200, [pedra, fogo])
 run(globals())
 
 
