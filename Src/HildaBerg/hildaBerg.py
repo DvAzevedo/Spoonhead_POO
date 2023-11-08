8378c841864d0b0ea16edcad603537cbcc35661e
Davi Azevedo <daviazevedos02@gmail.com>
2023-11-07 22:01:56 -0300

    Risada

--- Src/HildaBerg/hildaBerg.py
+++ Src/HildaBerg/hildaBerg.py
@@ -14,21 +14,57 @@ class Animate:
         self._contador.incrementa()
         self.file = self.imgs[self._contador_de_imagens._contador]
         self._contador_de_imagens.incrementa()
-        
 
+#Attaks
+
+#         self.helper += 0.1
+#         self.x -= 10
+#         self.y = ((math.sin(self.helperSeno)*75)+self.aux_y)
+class Ha(Image):
+    MAX_CONTADOR_UPDATES = 13
+    def __init__(self, x, y):
+        self.file = imgListHa[0]
+        self.animateHa = Animate(Ha.MAX_CONTADOR_UPDATES, imgListHa)
+        self.x = x
+        self.y = y
+        self.yo = y
+        self.sin = -1
+
+    def trajetoria(self):
+        self.sin += 1
+        self.x -= 60
+        self.y = ((math.sin(self.sin)*30)+self.yo)
+
+    def animate(self):
+        self.animateHa.animate()
+        self.file = self.animateHa.file
+
+    def destruir(self):
+        if self.x < -20:
+            self.destroy()
+
+    def update(self):
+        self.trajetoria()
+        self.animate()
+        self.destruir()
+
+
+
+
+#Hilda Build
 class HildaBergNormal(Image):
     MAX_CONTADOR_UPDATES = 21
+    MAX_CONTADOR_UPDATES_LOUGH = 13
     MAX_CONTADOR_INTRO_UPDATES = 43
     def __init__(self, x, y):
         self.file = hildaIntro[0]
-        self.estados = ["intro", "normal"]
+        self.estados = ["intro", "normal", "lough"]
         self.estado = self.estados[0]
         self.animateIntro = Animate(HildaBergNormal.MAX_CONTADOR_INTRO_UPDATES, hildaIntro)
         self.animateNormal = Animate(HildaBergNormal.MAX_CONTADOR_UPDATES, hildaNormal)
+        self.animateLough = Animate(HildaBergNormal.MAX_CONTADOR_UPDATES_LOUGH, hildaLaugh)
         self.x = x
         self.y = y
-        self._contador = Contador(HildaBergNormal.MAX_CONTADOR_UPDATES)
-        self._contador_de_imagens = Contador(21)
         self.count = 0
         self.i = 1.5
         
@@ -38,21 +74,37 @@ class HildaBergNormal(Image):
         self.y = (-a * math.sqrt(2) * math.cos(self.i) / (1 + math.sin(self.i)**2)) + 200
 
     def animate(self):
-        if self.estado == "normal":
-            self.animateNormal.animate()
-            self.file = self.animateNormal.file
-        elif self.estado == "intro":
+        if self.estado == "intro":
             self.animateIntro.animate()
             self.file = self.animateIntro.file
-            if self.count == 43:
+            if self.count == HildaBergNormal.MAX_CONTADOR_INTRO_UPDATES:
                 self.estado = "normal"
-    #def risada(self):            
+                self.count = 0
+
+        elif self.estado == "lough":
+            self.animateLough.animate()
+            self.file = self.animateLough.file
+            if self.count == HildaBergNormal.MAX_CONTADOR_UPDATES_LOUGH:
+                self.estado = "normal"
+                self.count = 0
+
+        elif self.estado == "normal":
+            self.animateNormal.animate()
+            self.file = self.animateNormal.file
+
+    def risada(self):
+        if keyboard.is_key_just_down('h'):
+            self.estado = "lough" 
+            self.count = 0 
+            Ha(self.x, self.y)
+
     def update(self):
         self.count +=1
+        self.risada()
         self.animate()
         if self.estado == "normal":
             self.updatePosition()
-        if self.count == 500:
+        if self.count == 100:
             self._hide()
             HildaBergNormalT(self.x, self.y)
             self.destroy()
--- Src/HildaBerg/hildaImgLists.py
+++ Src/HildaBerg/hildaImgLists.py
@@ -1,14 +1,18 @@
 
-hildaIntro = ["../../Img/HildaNormal/Intro/blimp_intro_0001.png"]
-for i in range(2, 44):
+hildaIntro = []
+for i in range(1, 44):
     hildaIntro.append(f"../../Img/HildaNormal/Intro/blimp_intro_{i:04d}.png")
 
-hildaNormal = ["../../Img/HildaNormal/Idle/blimp_idle_0001.png"]
-for i in range(2, 22):
+hildaNormal = []
+for i in range(1, 22):
     hildaNormal.append(f"../../Img/HildaNormal/Idle/blimp_idle_{i:04d}.png")
 
-hildaTransition = ["../../Img/HildaNormal/TransitionToMoon/Start/blimp_morph_0001.png"]
-for i in range(2, 8):
+hildaLaugh = []
+for i in range(1, 20):
+    hildaLaugh.append(f"../../Img/HildaNormal/Laugh/blimp_shoot_{i:04d}.png")
+
+hildaTransition = []
+for i in range(1, 8):
     hildaTransition.append(f"../../Img/HildaNormal/TransitionToMoon/BoilA/blimp_morph_{i:04d}.png")
 for i in range(8, 10):
     hildaTransition.append(f"../../Img/HildaNormal/TransitionToMoon/Middle/blimp_morph_{i:04d}.png")
@@ -17,6 +21,10 @@ for i in range(10, 14):
 for i in range(14, 49):
     hildaTransition.append(f"../../Img/HildaNormal/TransitionToMoon/End/blimp_morph_{i:04d}.png")
 
-hildaMoon = ["../../Img/HildaMoon/Idle/blimp_moon_idle_0001.png"]
-for i in range(2, 17):
+hildaMoon = []
+for i in range(1, 17):
     hildaMoon.append(f"../../Img/HildaMoon/Idle/blimp_moon_idle_{i:04d}.png")
+
+imgListHa = []
+for i in range(1, 14):
+    imgListHa.append(f"../../Img/HildaNormal/Attaks/Ha/blimp_ha_{i:04d}.png")
