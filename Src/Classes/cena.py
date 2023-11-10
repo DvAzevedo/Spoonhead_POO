42a3f11bfb4f646f17572cc454ba400ba1132c7b
Davi Azevedo <daviazevedos02@gmail.com>
2023-11-10 13:28:27 -0300

    Add Class Cena

---
+++ Src/Classes/cena.py
@@ -0,0 +1,20 @@
+class Cena:
+    def __init__(self, viloes, heroi, viloes_attaks, heroi_attaks):
+        self.viloes = viloes
+        self.heroi = heroi
+        self.viloes_attaks = viloes_attaks
+        self.heroi_attaks = heroi_attaks
+
+    def getVidaHeroi(self):
+        return self.heroi.life
+    def getVidaVilao(self, vilao):
+        return vilao.life
+    def heroiFoiAtingido(self):
+        pass
+    def heroiSofreDano(self):
+        self.heroi.life -=1
+    def vilaoAtingido(self, vilao):
+        pass
+    def vilaoSofreDano(self, vilao):
+        vilao.life -= 1
+    
