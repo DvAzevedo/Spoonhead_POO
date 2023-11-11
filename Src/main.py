d00df097a8f98fec374dfbdad87ef2a3bd7f139a
Davi Azevedo <daviazevedos02@gmail.com>
2023-11-11 01:02:37 -0300

    Nova branch Tornado

--- Src/main.py
+++ Src/main.py
@@ -14,15 +14,6 @@ criar_cenario()
 musica_de_fundo = pygame.mixer.music.load('../music/this-time.mp3')
 pygame.mixer.music.play(-1)
 
-pedra = Arma(1, PedraSeno)
-fogo = Arma(1, FogoReto)
-pedray3 = Arma(1, PedraY3)
-pedray5 = Arma(1, PedraY5)
-
-pedra = Arma(1, PedraSeno)
-fogo = Arma(1, FogoReto)
-pedray3 = Arma(1, PedraY3)
-pedray5 = Arma(1, PedraY5)
 #vi = Vilao_test_1(800, 300, ["../Img/blimp_idle_0001.png", "../Img/blimp_idle_0001.png" ],200, [pedra, fogo, pedray3, pedray5])
 
 vi = HildaBerg(700, 220)
