97fd7cc05dc86fbbfdd8b6f17973cda656ed9060
fabioctosta <fabioctosta@gmail.com>
2023-11-13 01:39:07 -0300

    Adicionado ataque triple Shoot e ataque mini Bomb

--- Src/Classes/chaliceImgLists.py
+++ Src/Classes/chaliceImgLists.py
@@ -14,3 +14,25 @@ for i in range(1, 5):
 ChaliceExplosion = []
 for i in range(1, 28):
     ChaliceExplosion.append(f"../Img/Chalice/Special/Explosion/shmup_super_explode_{i:04d}.png")
+
+BulletMove = []
+for i in range(1,5):
+    BulletMove.append(f"../Img/Chalice/Bullet/chalice_shmup_3way_bullet_a_{i:04d}.png")
+
+BulletMove_typeA = []
+for i in range(1,5):
+    BulletMove_typeA.append(f"../Img/Chalice/Bullet/TripleShoot/TypeA/chalice_shmup_3way_bullet_a_{i:04d}.png")
+
+BulletMove_typeB = []
+for i in range(1,5):
+    BulletMove_typeB.append(f"../Img/Chalice/Bullet/TripleShoot/TypeB/chalice_shmup_3way_bullet_b_{i:04d}.png")
+
+BulletMove_typeC = []
+for i in range(1,5):
+    BulletMove_typeC.append(f"../Img/Chalice/Bullet/TripleShoot/TypeC/chalice_shmup_3way_bullet_c_{i:04d}.png")
+
+BulletDict = [BulletMove_typeA,BulletMove_typeB,BulletMove_typeC]
+
+MiniBombMove = []
+for i in range(1,9):
+    MiniBombMove.append(f"../Img/Chalice/Mini_Bomb/TypeA/weapon_minibomb_bullets_a_{i:04d}.png")
