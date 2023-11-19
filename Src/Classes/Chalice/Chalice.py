d3956683f332a4463ab7a8ceb81caf5338c62c0b
João Zeba <joaog.zeba@gmail.com>
2023-11-19 19:29:24 -0300

    Remoção de lixo; implementação de privacidade, props e tipagem; refatoração.

--- Src/Classes/Chalice/Chalice.py
+++ Src/Classes/Chalice/Chalice.py
@@ -1,5 +1,4 @@
 from tupy import keyboard
-import math, random
 from Classes.Animacao import *
 from Classes.bars_indicators import *
 from Classes.Chalice.Ataques.Tiro import Tiro
@@ -12,139 +11,198 @@ class Chalice(Personagem):
     QTD_IMAGENS_TRANSICAO_ESPECIAL = 18
     QTD_IMAGENS_ESPECIAL = 4
 
-    def __init__(self, vilao, vida = 3, x=240, y=240):
+    def __init__(self, oponente: Personagem, x=240, y=240, vida = 3):
         super().__init__(x, y, vida, HitBox(x, y, 50, 25))
-        self.k = 20
-        self.atk_c = 0
-        self.is_atk_possible = True
-        self.c = 0
-        self.vilao = vilao
-        self.animate_normal = Animacao(Chalice.QTD_IMAGENS_NORMAL, ChaliceNormal, 6)
-        self.atacando = False
-        self.animate_trasition_special = Animacao(Chalice.QTD_IMAGENS_TRANSICAO_ESPECIAL, ChaliceTransitionToSpecial, 2)
-        self.animacao_special = Animacao(Chalice.QTD_IMAGENS_ESPECIAL, ChaliceSpecial, 3)
-        self.animacao_atual = self.animate_normal
-        self.contador_tiro = Contador(7)
-        self.contador_bomb = Contador(4)
-        self.contador_test = Contador(3)
-        self.contador_aux = Contador(2)
-        self.contador_simple_shoot = Contador(4)
-        self.aux_alternate = False
-        self.attack_mode = 0
-        self.especial_mode = False
-        self.life = Chalice_Life_bar(vida)
-        self.last_attack_object = []
-        self.changed = False
+        self._oponente = oponente
+        self._animacao = Animacao(Chalice.QTD_IMAGENS_NORMAL, ChaliceNormal, 6)
+        self._animacaoAtual = self.animacao
+        self._animacaoDeTransicao = Animacao(Chalice.QTD_IMAGENS_TRANSICAO_ESPECIAL, ChaliceTransitionToSpecial, 2)
+        self._animacaoEspecial = Animacao(Chalice.QTD_IMAGENS_ESPECIAL, ChaliceSpecial, 3)
+        self._barraDeVida = Chalice_Life_bar(vida)
+        self._contadorAuxiliar = Contador(2)
+        self._contadorDeTiroSimples = Contador(4)
+        self._atacando = False
+        self._modoDeAtaque = 0
+        self._modoEspecial = False
+        #self.possibilidadeDeAtaque = True
+        self._velocidade = 20
+        
+    @property
+    def oponente(self) -> Personagem:
+        return self._oponente
     
-    def update(self) -> None:
-        self.change_attack_mode()
-        self.common_attack_bullet()
-        self.change_direction()
-        self.file = self.animacao_atual.anima()
-        if self.file == self.animate_trasition_special.ultimaImg:
-            self.animacao_atual = self.animacao_special
-        if self.animacao_atual == self.animacao_special and self._collides_with(self.vilao):
-            Explosao(self.x, self.y)
-            self.animacao_atual = self.animate_normal
-        if keyboard.is_key_just_down('k'):
-                self.life.decrease_hp()
-        self.corrige_attack_em_relacao_posicao()
-
-    def change_direction(self) -> None:
-        if self.animacao_atual == self.animate_normal or self.animacao_atual == self.animacao_special:
-            if keyboard.is_key_down('Left') and self.x >= 20:
-                self.x -= self.k
-            if keyboard.is_key_down('Right') and self.x <= 780:
-                self.x += self.k
-            if keyboard.is_key_down('Up') and self.y >= 20:
-                if self.especial_mode is False:
-                    self.y -= self.k
-            if keyboard.is_key_down('Down') and self.y <= 480:
-                if self.especial_mode is False:
-                    self.y += self.k
-        if self.animacao_atual == self.animate_normal:
-            if keyboard.is_key_down('e'):
-                self.atacando = False
-                self.animacao_atual = self.animate_trasition_special
-        self.hitbox.atualiza_posicao(self.x, self.y)
+    @oponente.setter
+    def oponente(self, oponente: Personagem) -> None:
+        self._oponente = oponente
+        pass
     
-    def change_attack_mode(self):
-        if keyboard.is_key_just_down('z'):
-            if not self.contador_aux.esta_zerado():
-                self.contador_aux.zera_contador()
-            if self.attack_mode == 0:
-                self.attack_mode = 1
-            elif self.attack_mode == 1:
-                self.attack_mode = 2
-            else:
-                self.attack_mode = 0
-
-    def common_attack_bullet(self) -> None:
+    @property
+    def animacao(self) -> Animacao:
+        return self._animacao
+    
+    @animacao.setter
+    def animacao(self, animacao: Animacao) -> None:
+        self._animacao = animacao
+        pass
+    
+    @property
+    def animacaoAtual(self) -> Animacao:
+        return self._animacaoAtual
+    
+    @animacaoAtual.setter
+    def animacaoAtual(self, animacao: Animacao) -> None:
+        self._animacaoAtual = animacao
+        pass
+    
+    @property
+    def animacaoDeTransicao(self) -> Animacao:
+        return self._animacaoDeTransicao
+    
+    @animacaoDeTransicao.setter
+    def animacaoDeTransicao(self, animacao: Animacao) -> None:
+        self._animacaoDeTransicao = animacao
+        pass
+    
+    @property
+    def animacaoEspecial(self) -> Animacao:
+        return self._animacaoEspecial
+    
+    @animacaoEspecial.setter
+    def animacaoEspecial(self, animacao: Animacao) -> None:
+        self._animacaoEspecial = animacao
+        pass
+    
+    @property
+    def barraDeVida(self) -> Chalice_Life_bar:
+        return self._barraDeVida
+    
+    @barraDeVida.setter
+    def barraDeVida(self, barraDeVida: Chalice_Life_bar) -> None:
+        self._barraDeVida = barraDeVida
+        pass
+    
+    @property
+    def contadorAuxiliar(self) -> Contador:
+        return self._contadorAuxiliar
+    
+    @contadorAuxiliar.setter
+    def contadorAuxiliar(self, contador: Contador) -> None:
+        self._contadorAuxiliar = contador
+        pass
+    
+    @property
+    def contadorDeTiroSimples(self) -> Contador:
+        return self._contadorDeTiroSimples
+    
+    @contadorDeTiroSimples.setter
+    def contadorDeTiroSimples(self, contador: Contador) -> None:
+        self._contadorDeTiroSimples = contador
+        pass
+    
+    @property
+    def atacando(self) -> bool:
+        return self._atacando
+    
+    @atacando.setter
+    def atacando(self, atacando: bool) -> None:
+        self._atacando = atacando
+        pass
+    
+    @property
+    def modoDeAtaque(self) -> int:
+        return self._modoDeAtaque
+    
+    @modoDeAtaque.setter
+    def modoDeAtaque(self, modoDeAtaque: int) -> None:
+        self._modoDeAtaque = modoDeAtaque
+        pass
+    
+    @property
+    def modoEspecial(self) -> bool:
+        return self._modoEspecial
+    
+    @modoEspecial.setter
+    def modoEspecial(self, modoEspecial: bool) -> None:
+        self._modoEspecial = modoEspecial
+        pass
+    
+    @property
+    def velocidade(self) -> int:
+        return self._velocidade
+    
+    @velocidade.setter
+    def velocidade(self, velocidade: int) -> None:
+        self._velocidade = velocidade
+        pass
+    
+    def ataca(self) -> None:
         if keyboard.is_key_just_down('space'):
-            if self.animacao_atual == self.animate_normal:
+            if self.animacaoAtual == self.animacao:
                 self.atacando = not self.atacando
         if self.atacando == True:
-            if self.attack_mode == 0:
-                self.simple_shoot()
-            if self.attack_mode == 1:
-                self.triple_shoot_attack()
-            if self.attack_mode == 2:
-                self.mini_bomb_attack()
-                self.contador_aux.zera_contador()
-
-    def simple_shoot(self):
-        # if self.contador_aux.esta_zerado():
-        if self.contador_test.esta_zerado():
-            if self.contador_simple_shoot.contador < 2:
-                b1 = Tiro(self.posX, self.posY, self.vilao, 0, 2, 0, 10, 50)
-                self.last_attack_object.insert(0, b1)  
-            else:
-                b1 = Tiro(self.posX, self.posY, self.vilao, 0, 2, 0, -15, 50)
-                self.last_attack_object.insert(0, b1)  
-            self.contador_simple_shoot.incrementa()
-        self.contador_test.incrementa()
-        # self.contador_aux.incrementa()
-
-    def triple_shoot_attack(self):
-        if self.contador_tiro.esta_zerado():
-            a_disp = 8
-            b2 = Tiro(self.posX, self.posY, self.vilao, a_disp, 0)
-            b1 = Tiro(self.posX, self.posY, self.vilao, 0, 2, 20)
-            b3 = Tiro(self.posX, self.posY, self.vilao, (-1) * a_disp, 1)
-        self.contador_tiro.incrementa()
-
-    def mini_bomb_attack(self):
-        if self.contador_bomb.esta_zerado():
-            if self.contador_aux.contador % 2 == 0:
-                #bomb1 = Mini_Bomb(self.x, self.y, self.vilao, random.randrange(-20,1,5), "A", random.randrange(25,35,5))
-                bomb1 = MiniBomba(self.posX, self.posY, self.vilao, random.randrange(-20,1,5), "A", random.randrange(25,35,5))
-            else:
-                #bomb2 = Mini_Bomb(self.x, self.y, self.vilao, random.randrange(0,21,5), "A", random.randrange(25,45,5))
-                bomb2 = MiniBomba(self.posX, self.posY, self.vilao, random.randrange(0,21,5), "A", random.randrange(25,45,5))
-            self.contador_aux.incrementa()
-        self.contador_bomb.incrementa()
-
-    def corrige_attack_em_relacao_posicao(self):
-        if len(self.last_attack_object) > 0:
-            if self.last_attack_object[0].mudou is False:
-                if self.x != (self.last_attack_object[0].posX + self.last_attack_object[0].deslocamentoX):
-                    self.last_attack_object[0].posX = self.x
-                    self.last_attack_object[0].mudou = True
-                if self.y != ((self.last_attack_object[0].posY + 10) or (self.last_attack_object[0].posY - 15)):
-                    self.last_attack_object[0].posY = (self.y + self.last_attack_object[0].deslocamentoY)
-                    self.last_attack_object[0].mudou = True
-                self.last_attack_object.clear()
+            if self.modoDeAtaque == 0:
+                Tiro.tiro_simples(self.contadorDeTiroSimples, self.posX, self.posY, self.oponente)
+            if self.modoDeAtaque == 1:
+                Tiro.tiro_triplo(self.posX, self.posY, self.oponente)
+            if self.modoDeAtaque == 2:
+                MiniBomba.bombardeio(self.contadorAuxiliar, self.posX, self.posY, self.oponente)
+                self.contadorAuxiliar.zera_contador()
     
+    def movimenta(self) -> None:
+        if self.animacaoAtual == self.animacao or self.animacaoAtual == self.animacaoEspecial:
+            if keyboard.is_key_down('Left') and self.x >= 20:
+                self.x -= self.velocidade
+            if keyboard.is_key_down('Right') and self.x <= 780:
+                self.x += self.velocidade
+            if keyboard.is_key_down('Up') and self.y >= 20:
+                if self.modoEspecial is False:
+                    self.y -= self.velocidade
+            if keyboard.is_key_down('Down') and self.y <= 480:
+                if self.modoEspecial is False:
+                    self.y += self.velocidade
+        if self.animacaoAtual == self.animacao:
+            if keyboard.is_key_down('e'):
+                self.atacando = False
+                self.animacaoAtual = self.animacaoDeTransicao
+        self.hitbox.atualiza_posicao(self.x, self.y)
+    
+    def troca_modo_de_ataque(self):
+        if keyboard.is_key_just_down('z'):
+            if not self.contadorAuxiliar.esta_zerado():
+                self.contadorAuxiliar.zera_contador()
+            if self.modoDeAtaque == 0:
+                self.modoDeAtaque = 1
+            elif self.modoDeAtaque == 1:
+                self.modoDeAtaque = 2
+            else:
+                self.modoDeAtaque = 0
+    
+    def update(self) -> None:
+        self.troca_modo_de_ataque()
+        self.ataca()
+        self.movimenta()
+        self.file = self.animacaoAtual.anima()
+        if self.file == self.animacaoDeTransicao.ultimaImg:
+            self.animacaoAtual = self.animacaoEspecial
+        if self.animacaoAtual == self.animacaoEspecial and self._collides_with(self.oponente):
+            Explosao(self.x, self.y)
+            self.animacaoAtual = self.animacao
+        if keyboard.is_key_just_down('k'):
+                self.barraDeVida.decrease_hp()
+        Tiro.corrige_origem(self.posX, self.posY)
+        pass
+    
+    '''
     def flee(self) -> None:
         if keyboard.is_key_just_down('q'):
             self.y -= 20
-    # Break temporary your character attack
-    def b_atk(self,status: bool):
-        self.is_atk_possible = status
+            
+    def break_attack(self,status: bool):
+        self.possibilidadeDeAtaque = status
 
     def especial_movement(self) -> None:
         self.x += self.especial_vel
-        
+    '''
 class Explosao(Image):
     QTD_IMAGENS_EXPLOSAO = 27
     def __init__(self, x,y):
