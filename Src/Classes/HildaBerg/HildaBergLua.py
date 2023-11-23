c8e92b50a039efb837d749f2ad9cfbcef90ecc33
Davi Azevedo <daviazevedos02@gmail.com>
2023-11-23 19:20:39 -0300

    Listas de atks

--- Src/Classes/HildaBerg/HildaBergLua.py
+++ Src/Classes/HildaBerg/HildaBergLua.py
@@ -5,9 +5,23 @@ from Classes.HildaBerg.listasDeImagens import *
 from Classes.HildaBerg.trajetoria import *
 from Classes.bars_indicators import *
 
+QTD_IMG_STATE_NORMAL = 16
+QTD_IMG_STATE_ATK_INTRO = 12
+QTD_IMG_STATE_ATK = 8
+QTD_IMG_STATE_ATK_BACK = 8
+QTD_IMG_STATE_SMOKE = 15
+QTD_IMG_STATE_DEATH = 16
+ANIME_DELAY = 2
+
+idleAnime = Animacao(QTD_IMG_STATE_NORMAL, hildaMoon, ANIME_DELAY)
+atkIntroAnime = Animacao(QTD_IMG_STATE_ATK_INTRO, moonAtkIntro, ANIME_DELAY)
+atkAnime = Animacao(QTD_IMG_STATE_ATK, moonAtk, ANIME_DELAY)
+atkBackAnime = Animacao(QTD_IMG_STATE_ATK_BACK, moonAtkBack, ANIME_DELAY)
+smokeAnime = Animacao(QTD_IMG_STATE_SMOKE, moonSmoke, ANIME_DELAY)
+deathAnime = Animacao(QTD_IMG_STATE_DEATH, moonDeath, ANIME_DELAY)
+
 class HildaBergLua(Personagem):
-    QTD_IMG_STATE_NORMAL = 16
-    ANIME_DELAY = 2
+    
     STATE = 0
     
     def __init__(self, x, y):
@@ -15,7 +29,6 @@ class HildaBergLua(Personagem):
         self.imgs = hildaMoon
         self.x = x
         self.y = y
-        self.normalAnime = Animacao(HildaBergLua.QTD_IMG_STATE_NORMAL, hildaMoon, HildaBergLua.ANIME_DELAY)
         self.count = 0
 
     def ataca(self):
@@ -25,5 +38,5 @@ class HildaBergLua(Personagem):
         return super().movimenta()
 
     def update(self):
-        self.normalAnime.animar()
-        self.file = self.normalAnime.imagem
+        idleAnime.animar()
+        self.file = idleAnime.imagem
