9edc7fce886546bd110a71c1f9f913c9b8f70afa
João Zeba <joaog.zeba@gmail.com>
2023-11-26 03:43:40 -0300

    Traduzindo. Removendo lixo. Atualizando Hitbox.

--- Src/Classes/HildaBerg/HildaBergLua.py
+++ Src/Classes/HildaBerg/HildaBergLua.py
@@ -1,22 +1,17 @@
 from tupy import*
 from Classes.Animacao import Animacao
-from Classes.Personagem import Personagem
-from Classes.HildaBerg.listasDeImagens import *
-from Classes.HildaBerg.trajetoria import *
 from Classes.bars_indicators import *
+from Classes.Personagem import *
+from Classes.HildaBerg.listasDeImagens import hildaMoon
 
 class HildaBergLua(Personagem):
-    QTD_IMG_STATE_NORMAL = 16
-    ANIME_DELAY = 2
-    STATE = 0
+    QTD_IMAGENS = 16
+    ATRASO_DE_ANIMACAO = 2
     
-    def __init__(self, x, y):
+    def __init__(self, x: int, y: int, vida: int, hitbox: HitBox) -> None:
+        super().__init__(x, y, vida, hitbox)
         self.file = hildaMoon[0]
-        self.imgs = hildaMoon
-        self.x = x
-        self.y = y
-        self.normalAnime = Animacao(HildaBergLua.QTD_IMG_STATE_NORMAL, hildaMoon, HildaBergLua.ANIME_DELAY)
-        self.count = 0
+        self.normalAnime = Animacao(HildaBergLua.QTD_IMAGENS, hildaMoon, HildaBergLua.ATRASO_DE_ANIMACAO)
 
     def ataca(self):
         return super().ataca()
@@ -27,3 +22,4 @@ class HildaBergLua(Personagem):
     def update(self):
         self.normalAnime.animar()
         self.file = self.normalAnime.imagem
+        self.hitbox.atualiza_posicao(self.posX - 85, self.posY - 60)
