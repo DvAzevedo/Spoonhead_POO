8850cc55b5706c72637564628f1cb22b36db8d50
João Zeba <joaog.zeba@gmail.com>
2023-11-26 23:14:34 -0300

    Restaurando música

--- Src/main.py
+++ Src/main.py
@@ -7,6 +7,9 @@ from Classes.InicioDeJogo import *
 
 pygame.init()
 
+pygame.mixer.music.load('Sound/Music/MusicaDoMenu.mp3')
+pygame.mixer.music.play(-1)
+
 Cenario.criar_cenario()
 
 mensagemDeReady = Ready()
